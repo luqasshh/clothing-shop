2f7f9b26d311c936fb3cbe769d7456698ea88909
Łukasz <luqasshh@gmail.com>
2022-07-06 18:52:22 +0200

    authentication

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "firebase/app";
+import { initializeFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyAqgEHWIqPEoNuoDVMe8BuMJdNpWpba-w8",
+  authDomain: "amazing-clothing-db.firebaseapp.com",
+  projectId: "amazing-clothing-db",
+  storageBucket: "amazing-clothing-db.appspot.com",
+  messagingSenderId: "995442874775",
+  appId: "1:995442874775:web:df16e9f730cc72775bccd2",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+const db = initializeFirestore(firebaseApp, { useFetchStreams: false });
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+  console.log(userSnapShot);
+  console.log(userSnapShot.exists());
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating user, ", error);
+    }
+  }
+  return userDocRef;
+};
